418395b9ff8420766582f508d1830356a0c368d3
mohammedek <mohammedek159@gmail.com>
2022-07-08 11:12:33 +0530

    changes in home_screen.dart file

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:nss_wmo/widgets/photoItem.dart';
 import 'event_page.dart';
 
 class HomeScreen extends StatelessWidget {
