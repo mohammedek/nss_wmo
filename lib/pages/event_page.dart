c5442539129f5b116a4638f003cb4c1279158e70
mohammedek <mohammedek159@gmail.com>
2022-08-09 00:15:36 +0530

    test2 stream builder

--- lib/pages/event_page.dart
+++ lib/pages/event_page.dart
@@ -1,73 +1,64 @@
+
+import 'dart:html';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:nss_wmo/pages/home_screen.dart';
 
 import '../widgets/FirebaseStoreDataBase.dart';
+ class EventPage extends StatefulWidget {
+   const EventPage({Key? key}) : super(key: key);
 
-class EventPage extends StatelessWidget {
-  const EventPage({Key? key}) : super(key: key);
+   @override
+   State<EventPage> createState() => _EventPageState();
+ }
 
-  @override
-  Widget build(BuildContext context) {
+ class _EventPageState extends State<EventPage> {
+   @override
+   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              leading: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Icon(Icons.arrow_back)),
-              title: Text('NSS Daily Events'),
-            ),
-            body: StreamBuilder(
-                stream:
-                    FirebaseFirestore.instance.collection("events").snapshots(),
-                builder: (context, snapshots) {
-                  return ListView.builder(
-                      itemCount: snapshots.data.documents.length,
-                      itemBuilder: (context, index) {
-                        DocumentSnapshot events =
-                            snapshots.data.documents[index];
-                        return ListView(
-                            physics: AlwaysScrollableScrollPhysics(),
-                            children: <Widget>[
-                              Card(
-                                  elevation: 50,
-                                  shadowColor: Colors.black,
-                                  color: Colors.blueAccent[100],
-                                  child: SizedBox(
-                                    width: 300,
-                                    height: 300,
-                                    child: Padding(
-                                        padding: const EdgeInsets.all(20.0),
-                                        child: Image.network(events['img'])),
-                                  )),
-                              Card(
-                                elevation: 50,
-                                shadowColor: Colors.black,
-                                color: Colors.blueAccent[100],
-                                child: SizedBox(
-                                  width: 300,
-                                  height: 300,
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(20.0),
-                                    child: Image(
-                                      image: NetworkImage(
-                                        "https://firebasestorage.googleapis.com/v0/b/nss-website-wmo.appspot.com/o/%E2%80%94Pngtree%E2%80%94indian%20ten%20rupee%20coin%20vector_8133288.png?alt=media&token=2ec8cdd7-f872-4ed4-b727-cc0d2eea1431",
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ]);
-                      });
-                }
-                )
-        )
+      home: Scaffold(
+        appBar: AppBar(
+          leading: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Icon(Icons.arrow_back)),
+          title: Text('NSS Daily Events'),
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+          // Renders every post from the Firebase Firestore Database.
+            stream: FirebaseFirestore.instance.collection("events").snapshots(),
+            builder:
+                (BuildContext context,
+                AsyncSnapshot snapshot) {
+              if (snapshot.hasError) {
+                return Text('Something went wrong');
+              }
+
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator(
+                );
+              }
+
+              return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                   return ListTile(
+                      // Renders the title on every single listview.
+                      title: new Text(snapshot.data('name')),
+                      leading: new Image.network(snapshot.data('img')),
+                    );
+                  });
+            }
+        ),
+      ),
     );
   }
 }
+
+
 
 class ImageLoader extends StatelessWidget {
   const ImageLoader({Key? key}) : super(key: key);
